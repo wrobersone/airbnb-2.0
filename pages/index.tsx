5d6eacec93df6447850d5d3408633ac5e1ff8de3
William Robersone <wmrobersone@yahoo.com>
2022-11-28 20:12:08 -0600

    cardsData + exploreData

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
 import Footer from "../components/Footer";
 
-const Home: NextPage = ({ exploreData, cardData }) => {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -29,7 +29,7 @@ const Home: NextPage = ({ exploreData, cardData }) => {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {/* API endpoints */}
-            {exploreData.map(({ img, distance, location }) => (
+            {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
                 key={img}
                 img={img}
@@ -44,7 +44,7 @@ const Home: NextPage = ({ exploreData, cardData }) => {
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -m-3">
-            {cardData.map(({ img, title }) => (
+            {cardsData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
@@ -61,23 +61,21 @@ const Home: NextPage = ({ exploreData, cardData }) => {
       <Footer />
     </div>
   );
-};
+}
 
 export async function getStaticProps() {
   const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
     (res) => res.json()
   );
 
-  const cardData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
     (res) => res.json()
   );
 
   return {
     props: {
       exploreData,
-      cardData,
+      cardsData,
     },
   };
 }
-
-export default Home;
