9e3177ef67c452b4746898f682a88696376fd4c7
William Robersone <wmrobersone@yahoo.com>
2022-12-03 21:43:02 -0600

    package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ exploreData, cardsData }: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
+      <Header placeholder={"Start your search"} />
       <Banner />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
